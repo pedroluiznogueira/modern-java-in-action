a760b523e8b3ad825d0b2f446df01c3698bed996
pedroluiznogueira <peluiznogueiram@hotmail.com>
2022-04-19 02:03:07 -0300

    first commit

---
+++ src/main/java/com/github/pedroluiznogueira/java/eight/in/action/Application.java
@@ -0,0 +1,16 @@
+package com.github.pedroluiznogueira.java.eight.in.action;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("Running...");
+	}
+
+}
